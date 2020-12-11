6d077173756f28c76905be7e50d9687b28eda8c4
Allan Jonna <allandello22@gmail.com>
2020-12-11 23:53:39 +0700

    add background to navbar

--- src/Pages/Layout/NavBar.js
+++ src/Pages/Layout/NavBar.js
@@ -9,14 +9,22 @@ export default class NavBar extends Component {
 			<nav className='navBar'>
 				<div className='left-title'>My inventory app</div>
 				<div className='navbar-container'>
-					<div className='navbar-items'>
-						<Link to='/'>Home </Link>
+					<div>
+						<Link className='navbar-items' to='/'>
+							Home{' '}
+						</Link>
 					</div>
-					<div className='navbar-items'>
-						<Link to='/product'> Product </Link>
+					<div>
+						<Link className='navbar-items' to='/product'>
+							{' '}
+							Product{' '}
+						</Link>
 					</div>
-					<div className='navbar-items'>
-						<Link to='/categories'> Category </Link>
+					<div>
+						<Link className='navbar-items' to='/categories'>
+							{' '}
+							Category{' '}
+						</Link>
 					</div>
 				</div>
 			</nav>
